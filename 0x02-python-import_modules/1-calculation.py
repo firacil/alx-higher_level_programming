19f85c123bf25b5b3e25b4d35cbf4d3736948f05
firacil <firaolmekbib511@gmail.com>
2024-01-04 12:49:47 -0800

    calculate

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as cal
+
+a = 10
+b = 5
+
+print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
