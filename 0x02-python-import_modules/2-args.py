bc017fdacc45e3c489726683459de38023826311
firacil <firaolmekbib511@gmail.com>
2024-01-04 13:46:50 -0800

    command line

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+arg_no = len(sys.argv) - 1  # exclude script
+
+if arg_no == 0:
+    print('{:d} arguments.'.format(arg_no))
+elif arg_no == 1:
+    print('{:d} argument:'.format(arg_no))
+else:
+    print('{:d} arguments:'.format(arg_no))
+
+for i in range(1, len(sys.argv)):
+    print(f'{i}: {sys.argv[i]}')
