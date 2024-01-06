b54b5e519570378dda3532ad9d2694c5b867920a
firacil <firaolmekbib511@gmail.com>
2024-01-06 01:11:00 -0800

    replace list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,9 +3,9 @@
 def replace_in_list(my_list, idx, element):
 
     if idx < 0:
-        return
+        return my_list
     elif idx > len(my_list) - 1:
-        return
+        return my_list
     else:
         my_list[idx] = element
         return my_list
