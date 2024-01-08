7d5bcd088046db31ad3d72276e4ded28022085bc
firacil <firaolmekbib511@gmail.com>
2024-01-08 14:08:28 -0800

    add 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    new = ()
+    t1 = tuple_a + (0, 0)
+    t2 = tuple_b + (0, 0)
+    new = t1[0] + t2[0], t1[1] + t2[1]
+    return new
