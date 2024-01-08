7ddd7450c9903e693c6b0c96190eca98260f9fc1
firacil <firaolmekbib511@gmail.com>
2024-01-08 13:51:09 -0800

    string length

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if sentence != '':
-        f_char = sentence[0]
+        first_char = sentence[0]
     else:
-        f_char = None
-    return(len(sentence), f_char)
+        first_char = None
+    return(len(sentence), first_char)
