7c937f0fed18db525cb1998044d95d3551e30613
firacil <firaolmekbib511@gmail.com>
2024-01-08 13:35:38 -0800

    maximum integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if len(my_list) == 0:
+        return (None)
+    else:
+        maximum = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > maximum:
+                maximum = my_list[i]
+        return (maximum)
