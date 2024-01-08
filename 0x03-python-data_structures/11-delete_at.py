9a38d7750fbf8a20491c3c35fd70f5597cee338b
firacil <firaolmekbib511@gmail.com>
2024-01-08 10:47:03 -0800

    delete element

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    ''' function to delete element
+    at a given position'''
+
+    if idx < 0 or idx >= len(my_list):
+        return
+
+    if my_list:
+        del my_list[idx]
+        return my_list
