fa2589ba207794562613918e30b9591565f22fb1
firacil <firaolmekbib511@gmail.com>
2024-01-08 11:27:50 -0800

    divisible elements

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,10 +4,9 @@ def divisible_by_2(my_list=[]):
 
     newl = []
 
-    if my_list:
-        for i in my_list:
-            if i % 2 == 0:
-               newl.append(True)
-            else:
-                newl.append(False)
-        return (newl)
+    for i in my_list:
+        if i % 2 == 0:
+            newl.append(True)
+        else:
+            newl.append(False)
+    return (newl)
