c68956e218efc9c3f31a4f81d346dc778de62780
firacil <firaolmekbib511@gmail.com>
2024-01-22 10:28:33 -0800

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
 
+    tot = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
+            tot += 1
         except IndexError:
-            print()
-            return (i)
+            break
     print()
-    return(x)
+    return(tot)
