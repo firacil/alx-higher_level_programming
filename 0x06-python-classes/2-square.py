e97ee7fc45ea4de4396bc215f5dd0a4ee747125b
firacil <firaolmekbib511@gmail.com>
2024-01-24 15:11:54 -0800

    updated

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 0-square.py
+""" Define a class Square """
+
+
+class Square():
+    """ Describe a Square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square
+
+        Args:
+            size(int): the size of the square
+
+        Raises:
+            TypeError: when size failed to be number
+            ValueError: size must be >= 0
+
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be a number")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
