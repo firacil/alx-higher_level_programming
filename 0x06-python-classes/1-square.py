12171ff656ae091c7908bb7c26cbe4cc9761fa93
firacil <firaolmekbib511@gmail.com>
2024-01-24 15:03:45 -0800

    updated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+""" Define a class Square """
+
+
+class Square():
+    """ describe a square"""
+
+    def __init__(self, size):
+        """Intialize a Square
+
+        Args:
+            size(int): size of the square
+        """
+        self.__size = size
