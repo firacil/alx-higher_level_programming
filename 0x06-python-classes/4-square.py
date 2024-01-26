d9cce63d0a46cfb2019adf813e6b2388b1782b01
firacil <firaolmekbib511@gmail.com>
2024-01-26 05:58:02 -0800

    updated

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,14 +5,34 @@ class Square:
     """Defines a square based on 3-square.py"""
 
     def __init__(self, size=0):
+        """ Initialize the class
+
+        Args:
+            size(int): size of the square
+
+        """
         self.size = size
 
     @property
     def size(self):
+        """ this is getter for size of the square
+            it retrives size of the square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ setter for the size of square
+
+            Args:
+                value: value passed by the user
+            Raises:
+                TypeError: size must be integer
+                ValueError: size must be not less than 0
+
+            it checks wheteher value have to be passed or not
+            using conditions
+        """
 
         if isinstance(value, int):
             self.__size = value
@@ -23,5 +43,10 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """ function to calculate the area of square
+
+            Return:
+                    area of the square
+        """
 
         return self.size ** 2
