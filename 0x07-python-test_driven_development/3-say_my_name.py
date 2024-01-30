e048b8c2ee11cbd587b87ce16225217f941adf27
firacil <firaolmekbib511@gmail.com>
2024-01-30 10:27:54 -0800

    updated

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" defines function that print names"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ prints names based on condition"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
