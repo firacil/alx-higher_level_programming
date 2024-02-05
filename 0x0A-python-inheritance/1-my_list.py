9f33472e5ac743d8f44614a8906e759aedb77798
firacil <firaolmekbib511@gmail.com>
2024-02-05 15:23:24 -0800

    updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,5 +7,5 @@ class MyList(list):
 
     def print_sorted(self):
         ''' function that prints sorted list'''
-        sorted_list = sorted(self)
+        sorted_list = sorted(list(self))
         print(sorted_list)
