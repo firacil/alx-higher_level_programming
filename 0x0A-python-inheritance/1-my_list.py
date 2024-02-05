b38be8da0b335b0bfe7600e347af63c1cfb79e2a
firacil <firaolmekbib511@gmail.com>
2024-02-05 15:15:50 -0800

    updated

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' Class MyList defined'''
+
+
+class MyList(list):
+    ''' class MyList inherits parent class 'list"'''
+
+    def print_sorted(self):
+        ''' function that prints sorted list'''
+        sorted_list = sorted(self)
+        print(sorted_list)
