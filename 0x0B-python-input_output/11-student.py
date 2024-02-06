06125d56443ecb4c91af8caeb61e0bbca64fb18a
firacil <firaolmekbib511@gmail.com>
2024-02-06 14:29:32 -0800

    updated

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" module that define class Student"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """ instantiation of class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retreves a dictionary representation of a Student
+            if attrs is alist of strings, represents only attributes specifed
+            otherwise all of them
+        """
+        if (type(attrs) == list and
+                all(type(i) == str for i in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the student"""
+        for x, y in json.items():
+            setattr(self, k, v)
