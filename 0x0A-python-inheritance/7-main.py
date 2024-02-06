834c3ad170be4f1f1ef44c2abb7266bd77c450f9
firacil <firaolmekbib511@gmail.com>
2024-02-06 04:38:09 -0800

    updated

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -20,3 +20,8 @@ try:
     bg.integer_validator("distance", -4)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4, "her")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
