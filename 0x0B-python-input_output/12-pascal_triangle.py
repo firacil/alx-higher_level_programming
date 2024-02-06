5fca13905bcd12a68562b4a82498e02361abc9ff
firacil <firaolmekbib511@gmail.com>
2024-02-06 14:52:15 -0800

    updated

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,9 +11,9 @@ def pascal_triangle(n):
 
     triangle = []
     for i in range(n):
-        row = [1] * (i + 1) # intialize each row
+        row = [1] * (i + 1)  # intialize each row
         if i >= 2:
-            for j in range(1, i): # skip the first and last element of the row
+            for j in range(1, i):  # skip the first and last element of the row
                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
     return triangle
