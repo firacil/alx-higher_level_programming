5f79c5edffc72d9b0f251a79e8143c84720f2d1d
firacil <firaolmekbib511@gmail.com>
2024-02-05 16:00:18 -0800

    updated

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' define class BaseGeometry'''
+
+
+class BaseGeometry:
+    ''' create function thats raises an exception'''
+
+    def area(self):
+        '''function to raise exception Exception'''
+        raise Exception("area() is not implemented")
