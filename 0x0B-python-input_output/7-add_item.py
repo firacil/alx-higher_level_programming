97f71e1d28979d80318c097e1661d48c8f2c80e6
firacil <firaolmekbib511@gmail.com>
2024-02-06 13:49:19 -0800

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
-items.extend(sys.argv[1:])
-save_to_json_file(items, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
