dd74e78f921669371c55ef2d88ba466906e724d0
firacil <firaolmekbib511@gmail.com>
2024-02-06 11:44:01 -0800

    updated

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function to read a file"""
+
+
+def read_file(filename=""):
+    """ function that read passed file"""
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read())
