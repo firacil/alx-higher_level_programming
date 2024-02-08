d221e0fd092417537a74511eb0e5f4f1b86b706d
firacil <firaolmekbib511@gmail.com>
2024-02-08 11:09:50 -0800

    updated

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""program to import all functions from the file calcluator_1.py"""
+import calculator_1 as cal
+import sys
+
+argument_number = len(sys.argv) - 1
+
+opr = ("+": add, "*": mul, "-": sub, "/": div)
+
+if argument_number != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+else:
+    if sys.argv[2] not in list(opr.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, opr[sys.argv[2](a, b)))
