9cbcb4dd06c3f611965e1f8f2bea7f599bb439ac
firacil <firaolmekbib511@gmail.com>
2024-02-08 11:30:47 -0800

    updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """program to import all functions from the file calcluator_1.py"""
-from calculator_1 import add, sub, mul, div
-import sys
+if __name__ == "main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-argument_number = len(sys.argv) - 1
+    argument_number = len(sys.argv) - 1
 
-opr = {"+": add, "*": mul, "-": sub, "/": div}
+    opr = {"+": add, "*": mul, "-": sub, "/": div}
 
-if argument_number != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-else:
-    if sys.argv[2] not in list(opr.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
+    if argument_number != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, opr[sys.argv[2]](a, b)))
+    else:
+        if sys.argv[2] not in list(opr.keys()):
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{} {} {} = {}".format(a, sys.argv[2], b, opr[sys.argv[2]](a, b)))
