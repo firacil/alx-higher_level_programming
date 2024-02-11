1793709f6ae520eafe17fdcb1a110db6d9ef9f68
firacil <firaolmekbib511@gmail.com>
2024-02-11 08:22:27 -0800

    updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,3 +17,18 @@ class Square(Rectangle):
     def __str__(self):
         """ overload __str__ methon"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    @property
+    def size(self):
+        """ getter of size"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """ setter of size"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
