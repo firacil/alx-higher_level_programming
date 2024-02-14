574b4dc6c50953b8f85e51ddb342b196b58a4cb0
firacil <firaolmekbib511@gmail.com>
2024-02-14 02:13:30 -0800

    updated

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,36 +15,28 @@ class test_rectangle(unittest.TestCase):
         '''
         intialize instance width and height
         '''
-        self.r = Rectangle(4, 5)
+        self.rec = Rectangle(4, 5)
 
     def test_teardown(self):
         '''
             delete created instances
         '''
-        del self.r
+        self.rec = Rectangle(4, 5)
+        del self.rec
 
     def test_width(self):
         '''
             testing rectangles width getter
         '''
-        self.assertEqual(4, self.r.width)
+        self.rec = Rectangle(4, 5)
+        self.assertEqual(4, self.rec.width)
 
     def test_height(self):
         '''
             testing rectangles height getter
         '''
-        self.assertEqual(5, self.r.height)
-
-    def test_attr(self):
-        '''
-            testing attributes of rectangle
-        '''
-        rectangle = Rectangle(5, 10, 1, 2, 100)
-        self.assertEqual(rectangle.width, 5)
-        self.assertEqual(rectangle.height, 10)
-        self.assertEqual(rectangle.x, 1)
-        self.assertEqual(rectangle.y, 2)
-        self.assertEqual(rectangle.id, 100)
+        self.rec = Rectangle(4, 5)
+        self.assertEqual(5, self.rec.height)
 
     def test_area(self):
         '''
