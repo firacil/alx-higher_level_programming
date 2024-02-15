6cd853f0dee49b89379553f460986a53e34a044f
firacil <firaolmekbib511@gmail.com>
2024-02-15 02:41:16 -0800

    updated

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -40,3 +40,26 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s.size, 6)
         self.assertEqual(s.x, 33)
         self.assertEqual(s.y, 65)
+
+    def test_to_dictionary(self):
+        '''
+            testing method to dictionary
+        '''
+        s = Square(5, 32, 64, 89)
+        sq_dict = s.to_dictionary()
+        ex_dict = {'id': 89, 'size': 5, 'x': 32, 'y': 64}
+        self.assertEqual(sq_dict, ex_dict)
+
+    def test_size_type(self):
+        '''
+            testing TypeError
+        '''
+        with self.assertRaises(TypeError):
+            s = Square("testing")
+
+    def test_size_value(self):
+        '''
+            testing when unwanted value occured
+        '''
+        with self.assertRaises(ValueError):
+            s = Square(-5)
