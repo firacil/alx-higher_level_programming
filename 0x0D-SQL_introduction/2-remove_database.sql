7da873068fe03fa60793fef10c22b07948bf74de
firacil <firaolmekbib511@gmail.com>
2024-03-12 07:14:41 -0700

    Remove DB from server

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- script that deletes the database hbtn_0c_0
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0
