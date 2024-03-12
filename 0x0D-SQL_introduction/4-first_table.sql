bb00b02569e5aadc517af4fbcc16047a5475ee93
firacil <firaolmekbib511@gmail.com>
2024-03-12 07:31:01 -0700

    TABLE CREATED

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -6,5 +6,4 @@
 -- If the table first_table already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 
-USE hbtn_0c_0
-CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
