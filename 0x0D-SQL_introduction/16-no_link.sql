bf94a0471cb04a874b01e81e96635aaaa86fd76c
firacil <firaolmekbib511@gmail.com>
2024-03-12 08:52:35 -0700

    list all recordes of the table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -4,4 +4,4 @@
 -- Records should be listed by descending score
 -- The database name will be passed as an argument to the mysql command
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
