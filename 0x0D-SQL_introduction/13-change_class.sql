ab50485c014d12e66244906f9f1be30c9004fc55
firacil <firaolmekbib511@gmail.com>
2024-03-12 08:29:23 -0700

    Remove too Low Score

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- cript that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+
+DELETE FROM second_table WHERE score <= 5
