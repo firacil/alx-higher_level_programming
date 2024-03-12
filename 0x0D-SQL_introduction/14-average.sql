bd235291d728b98268fa011b1a7041860187508e
firacil <firaolmekbib511@gmail.com>
2024-03-12 08:34:46 -0700

    Average of all records in the table

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -2,4 +2,4 @@
 -- The result column name should be average
 -- The database name will be passed as an argument of the mysql command
 
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) as average FROM second_table;
