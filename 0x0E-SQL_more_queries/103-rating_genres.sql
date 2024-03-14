eb6c05f4d34155a327a4b624d3f03e429804838d
firacil <firaolmekbib511@gmail.com>
2024-03-14 14:35:23 -0700

    updated_solutions

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -10,6 +10,6 @@ FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings
-ON tv_show_genres.show.id = tv_show_ratings.show_id
+ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.id
 ORDER BY rating DESC;
