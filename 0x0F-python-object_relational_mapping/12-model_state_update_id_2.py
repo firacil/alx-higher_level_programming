dee3dd9aefd5e309d6804df5fb9cc23c886ab385
firacil <firaolmekbib511@gmail.com>
2024-04-18 04:35:06 -0700

    updated

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    script that state passed as
+    an argument from the database
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+        we are going to create engine
+        and the have a new session to query
+        on the database
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    update_state = session.query(State).filter_by(id=2).first()
+
+    update_state.name = "New Mexico"
+
+    session.commit()
+
+    session.close()
