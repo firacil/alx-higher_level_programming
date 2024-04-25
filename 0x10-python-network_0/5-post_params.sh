c14865e9358769abc985d6ae1e3a23c8f80a8d45
firacil <firaolmekbib511@gmail.com>
2024-04-25 15:32:32 -0700

    updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -X POST -d "email: test@gmail.com" "subject: I will always be here for PLD" "$1"
+curl -s -X POST -d "email: test@gmail.com&subject: I will always be here for PLD" "$1"
