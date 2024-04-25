23fe39223281a29b3efea12e53c048430dbf3ef7
firacil <firaolmekbib511@gmail.com>
2024-04-25 15:04:29 -0700

    updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" | grep -o -m 1 'HTTP/1.1 200 OK' && curl -s "$1"
+curl -sL "$1"
