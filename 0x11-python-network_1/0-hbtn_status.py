5cc7baa725daad47862c4752aaaa235ecf2090ff
firacil <firaolmekbib511@gmail.com>
2024-04-30 04:08:00 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,13 @@
     script that fetchs http request
 """
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
 
-print('Body response:')
-print(f'    - type: {type(content)}')
-print(f'    - content: {content}')
-print(f'    - utf8 content: {utf8_content}')
+if __name__ == "__main__":
+    url = ('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print('Body response:')
+        print(f'    - type: {type(content)}')
+        print(f'    - content: {content}')
+        print(f'    - utf8 content: {utf8_content}')
