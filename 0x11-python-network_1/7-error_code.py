f8253cb7796e77570f15187f37c96a9fa2e0363c
firacil <firaolmekbib511@gmail.com>
2024-04-30 14:37:17 -0700

    updated

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,11 +7,10 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
+    url = argv[1]
 
-    try:
-        url = argv[1]
-        r = requests.get(url)
-        print(r.text)
-
-    except requests.exceptions.HTTPError as err:
+    r = requests.get(url)
+    if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
