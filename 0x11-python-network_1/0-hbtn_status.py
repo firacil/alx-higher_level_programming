0f0833fff767ba34c1b7625eeb48c50cc3531209
firacil <firaolmekbib511@gmail.com>
2024-04-30 04:16:44 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(url) as response:
         content = response.read()
-        utf8_content = content.decode('utf-8')
         print('Body response:')
-        print(f'    - type: {type(content)}')
-        print(f'    - content: {content}')
-        print(f'    - utf8 content: {utf8_content}')
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
