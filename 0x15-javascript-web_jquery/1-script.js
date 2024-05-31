aa91ab937b2e5c19f6aba7b0a56da5de54722ec2
Firaol Mk <firaolmekbib511@gmail.com>
2024-05-30 21:35:54 -0700

    updated

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,3 +1,3 @@
 // changing color of header using jquery
-let headerElement = document.querySelector('header');
+const headerElement = document.querySelector('header');
 $(headerElement).css('color', '#FF0000');
