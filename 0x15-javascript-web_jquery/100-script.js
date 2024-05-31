d1b14cb6111a57cc6bd86fdfd37d8fe22f5c3a18
Firaol Mk <firaolmekbib511@gmail.com>
2024-05-31 05:51:48 -0700

    updated

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// changin color of header content when script imported from head
+document.addEventListener('DOMContentLoaded', function () {
+  const headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
