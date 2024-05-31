6559cf6afee04610290ae2afd7381404c07e0d71
Firaol Mk <firaolmekbib511@gmail.com>
2024-05-31 03:59:11 -0700

    updated

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  // by using get method we will fetch data in the provided url
+  // then display in our page
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    const characterName = data.name;
+    $('#character').text(characterName);
+  });
+});
