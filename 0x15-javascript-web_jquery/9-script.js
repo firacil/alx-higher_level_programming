2f33f6ade7b53f7a11efcc79041878afebb418d5
Firaol Mk <firaolmekbib511@gmail.com>
2024-05-31 10:35:31 -0700

    updated

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,5 @@
 // retrieving data from the url using get method
+/* global $ */
 $(document).ready(function () {
   $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
     $('#hello').text(data.hello);
