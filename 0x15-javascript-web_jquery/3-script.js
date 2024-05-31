eb86c4ab3194e1facf8c3766fff31d05a3719480
Firaol Mk <firaolmekbib511@gmail.com>
2024-05-30 22:46:06 -0700

    updated

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,8 @@
+// script to add class red to header
+$(document).ready(function () {
+  // select the elements and adding click event
+  $('#red_header').click(function () {
+    // add class red to header
+    $('header').addClass('red');
+  });
+});
