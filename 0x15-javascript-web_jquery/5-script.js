077d52018e16d37ee7122b80010f3c6c7f104120
Firaol Mk <firaolmekbib511@gmail.com>
2024-05-30 23:44:36 -0700

    updated

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,11 @@
+// adding new item using jquery
+$(document).ready(function () {
+  // select element and add click event
+  $('#add_item').click(function () {
+    // create variable to hold element
+    const newItem = $('<li>Item</li>');
+
+    // add new item to ul
+    $('ul.my_list').append(newItem);
+  });
+});
