48c466d8096fb86e640e0763b1ee224f841a488e
Firaol Mk <firaolmekbib511@gmail.com>
2024-05-30 23:18:16 -0700

    updated

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+// script that update class of the header
+$(document).ready(function () {
+  // select elements using jquery and set click event
+  $('#toggle_header').click(function () {
+    // check if class is red or green
+    if ($('header').hasClass('green')) { $('header').removeClass('green').addClass('red'); } else { $('header').removeClass('red').addClass('green'); }
+  });
+});
